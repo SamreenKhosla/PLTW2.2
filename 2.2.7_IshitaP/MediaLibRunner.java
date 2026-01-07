2d7f7f9335bf8365093a53b95209ab3d8510babf
IshitaPrasad1 <prasad1449@mydusd.org>
2026-01-07 13:40:25 -0800

    Add files via upload

---
+++ 2.2.7_IshitaP/MediaLibRunner.java
@@ -0,0 +1,14 @@
+/*
+ * Activity 2.2.7
+ * 
+ * The runner for the MediaLib program
+ */
+public class MediaLibRunner
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to your Media Library");
+    MediaLib myLib = new MediaLib();
+
+  }
+}
